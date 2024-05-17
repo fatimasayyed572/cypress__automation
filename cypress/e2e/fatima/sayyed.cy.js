e2eeb97abea69f2147f4b96bcef731633acc4b02
fatimasayyed572 <fatimasayyed572@gmail.com>
2024-05-17 13:06:47 +0530

    Testing

---
+++ cypress/e2e/fatima/sayyed.cy.js
@@ -0,0 +1,11 @@
+describe("testing", () => {
+
+    it("test it 01", () => {
+        cy.log("Testong log")
+        cy.visit("https://www.saucedemo.com/v1/index.html")
+        cy.get("#user-name").   type("standard_user")
+        cy.get("#password").type("secret_sauce")
+        cy.get("#login-button").click()
+    })
+
+})
